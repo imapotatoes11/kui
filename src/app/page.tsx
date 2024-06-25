f395c43d08db4567fa40fa8c0a3e05689cc46eb2
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2024-06-25 18:53:01 -0400

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,16 @@ import Card from "@/components/Card";
 import ConfigButton from "@/components/ConfigButton";
 import ContrastIcon from '@mui/icons-material/Contrast'
 
-const size: string = "medium"
-
 export default function Home() {
     // @ts-ignore
     return (
         <main className="flex items-center justify-center h-screen bg-gray-100 flex-col gap-5">
             <div className="bg-white shadow-lg rounded-lg p-5 flex-wrap gap-2.5 flex justify-center">
-                <Button size={`${size}`} color={"primary"}>aa</Button>
-                <Button size={`${size}`} color={"secondary"}>aa</Button>
-                <Button size={`${size}`} color={"tertiary"}>aa</Button>
-                <Button size={`${size}`} color={"outline"}>aa</Button>
-                <Button size={`${size}`} color={"none"}>aa</Button>
+                <Button color={"primary"}>aa</Button>
+                <Button color={"secondary"}>aa</Button>
+                <Button color={"tertiary"}>aa</Button>
+                <Button color={"outline"}>aa</Button>
+                <Button color={"none"}>aa</Button>
             </div>
             <Card>
                 aa
